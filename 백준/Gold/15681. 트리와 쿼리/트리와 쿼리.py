ef16345d3f1abe4f8b42b8e66ffddea5d568758c
JungWook Mun <68419544+m020202@users.noreply.github.com>
2024-07-13 23:05:46 +0900

    [Gold V] Title: 트리와 쿼리, Time: 3420 ms, Memory: 384440 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.py
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.py
@@ -1,37 +1,21 @@
-from collections import deque
 import sys
 sys.setrecursionlimit(120000)
 n,r,q = map(int,input().split())
 box = [[] for _ in range(n+1)]
-tree = [[] for _ in range(n+1)]
-dQ = deque()
+visit = [-1] * (n+1)
 for _ in range(n-1):
     u,v = map(int,input().split())
     box[u].append(v)
     box[v].append(u)
 
-ch = [-1] * (n+1)
-ch[r] = 0
-dQ.append(r)
-while dQ:
-    cur = dQ.popleft()
+def dfs(cur):
+    visit[cur] = 1
     for i in box[cur]:
-        if ch[i] == -1:
-            tree[cur].append(i)
-            ch[i] = 0
-            dQ.append(i)
+        if visit[i] == -1:
+            visit[cur] += dfs(i)
+    return visit[cur]
 
-sub = [-1] * (n+1)
-def find(cur):
-    if sub[cur] != -1:
-        return sub[cur]
-    ans = 1
-    for i in tree[cur]:
-        ans += find(i)
-    sub[cur] = ans
-    return ans
-find(r)
-
+dfs(r)
 for _ in range(q):
-    cur = int(input())
-    print(sub[cur])
+    now = int(input())
+    print(visit[now])
