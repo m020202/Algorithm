5b7596682bcca9a26607023905143f424116c69a
JungWook Mun <68419544+m020202@users.noreply.github.com>
2024-07-20 23:17:40 +0900

    [Gold V] Title: 동전 1, Time: 188 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.py
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -1,13 +1,10 @@
-import sys
-input = sys.stdin.readline
 n,k = map(int,input().split())
 box = [int(input()) for _ in range(n)]
 box.sort()
-ch = [0] * (k+1)
-ch[0] = 1
+dp = [0] * (k+1)
+dp[0] = 1
 for i in box:
-    for j in range(1,k+1):
-        if j >= i:
-            ch[j] += ch[j-i]
+    for j in range(i,k+1):
+        dp[j] += dp[j-i]
 
-print(ch[k])
+print(dp[-1])
