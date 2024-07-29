8dc8bf066c6b736f1082f438bcdd35508f6d2035
JungWook Mun <68419544+m020202@users.noreply.github.com>
2024-07-29 11:36:29 +0900

    [Gold IV] Title: RGB거리 2, Time: 128 ms, Memory: 110408 KB -BaekjoonHub

--- 백준/Gold/17404. RGB거리 2/RGB거리 2.py
+++ 백준/Gold/17404. RGB거리 2/RGB거리 2.py
@@ -5,85 +5,24 @@ box = [tuple(map(int,input().split())) for _ in range(n)]
 dp = [[0]*3 for _ in range(n)]
 ans = INF
 
-# 0 1
-dp[0][0] = box[0][0]
-dp[0][1] = INF
-dp[0][2] = INF
-for i in range(1,n-1):
-    # 빨강
-    dp[i][0] = box[i][0] + min(dp[i-1][1], dp[i-1][2])
-    # 초록
-    dp[i][1] = box[i][1] + min(dp[i-1][0], dp[i-1][2])
-    # 파랑
-    dp[i][2] = box[i][2] + min(dp[i-1][0], dp[i-1][1])
+def DP(num,end):
+    global ans, dp
+    dp[0][num] = box[0][num]
+    dp[0][(num+1)%3] = INF
+    dp[0][(num+2)%3] = INF
 
-ans = min(ans, box[-1][1] + dp[n-2][0], box[-1][1] + dp[n-2][2])
+    for i in range(1,n-1):
+        # 빨강
+        dp[i][0] = box[i][0] + min(dp[i-1][1], dp[i-1][2])
+        # 초록
+        dp[i][1] = box[i][1] + min(dp[i-1][0], dp[i-1][2])
+        # 파랑
+        dp[i][2] = box[i][2] + min(dp[i-1][0], dp[i-1][1])
+    
+    ans = min(ans, box[-1][end] + dp[n-2][(end+1)%3], box[-1][end] + dp[n-2][(end+2)%3])
 
-# 0 2
-dp[0][0] = box[0][0]
-dp[0][1] = INF
-dp[0][2] = INF
-for i in range(1,n-1):
-    # 빨강
-    dp[i][0] = box[i][0] + min(dp[i-1][1], dp[i-1][2])
-    # 초록
-    dp[i][1] = box[i][1] + min(dp[i-1][0], dp[i-1][2])
-    # 파랑
-    dp[i][2] = box[i][2] + min(dp[i-1][0], dp[i-1][1])
-
-ans = min(ans, box[-1][2] + dp[n-2][0], box[-1][2] + dp[n-2][1])
-
-# 1 0
-dp[0][1] = box[0][1]
-dp[0][0] = INF
-dp[0][2] = INF
-for i in range(1,n-1):
-    # 빨강
-    dp[i][0] = box[i][0] + min(dp[i-1][1], dp[i-1][2])
-    # 초록
-    dp[i][1] = box[i][1] + min(dp[i-1][0], dp[i-1][2])
-    # 파랑
-    dp[i][2] = box[i][2] + min(dp[i-1][0], dp[i-1][1])
-
-ans = min(ans, box[-1][0] + dp[n-2][1], box[-1][0] + dp[n-2][2])
-# 1 2
-dp[0][1] = box[0][1]
-dp[0][0] = INF
-dp[0][2] = INF
-for i in range(1,n-1):
-    # 빨강
-    dp[i][0] = box[i][0] + min(dp[i-1][1], dp[i-1][2])
-    # 초록
-    dp[i][1] = box[i][1] + min(dp[i-1][0], dp[i-1][2])
-    # 파랑
-    dp[i][2] = box[i][2] + min(dp[i-1][0], dp[i-1][1])
-
-ans = min(ans, box[-1][2] + dp[n-2][0], box[-1][2] + dp[n-2][1])
-# 2 0
-dp[0][2] = box[0][2]
-dp[0][0] = INF
-dp[0][1] = INF
-for i in range(1,n-1):
-    # 빨강
-    dp[i][0] = box[i][0] + min(dp[i-1][1], dp[i-1][2])
-    # 초록
-    dp[i][1] = box[i][1] + min(dp[i-1][0], dp[i-1][2])
-    # 파랑
-    dp[i][2] = box[i][2] + min(dp[i-1][0], dp[i-1][1])
-
-ans = min(ans, box[-1][0] + dp[n-2][1], box[-1][0] + dp[n-2][2])
-# 2 1
-dp[0][2] = box[0][2]
-dp[0][0] = INF
-dp[0][1] = INF
-for i in range(1,n-1):
-    # 빨강
-    dp[i][0] = box[i][0] + min(dp[i-1][1], dp[i-1][2])
-    # 초록
-    dp[i][1] = box[i][1] + min(dp[i-1][0], dp[i-1][2])
-    # 파랑
-    dp[i][2] = box[i][2] + min(dp[i-1][0], dp[i-1][1])
-
-ans = min(ans, box[-1][1] + dp[n-2][0], box[-1][1] + dp[n-2][2])
-
+for i in range(3):
+    DP(i,(i+1)%3)
+    DP(i,(i+2)%3)
+    
 print(ans)
