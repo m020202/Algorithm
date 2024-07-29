03d3f3405f4b42aaa1ac837e088dddf84ad0786f
JungWook Mun <68419544+m020202@users.noreply.github.com>
2024-07-29 11:40:55 +0900

    [Gold IV] Title: RGB거리 2, Time: 84 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/17404. RGB거리 2/RGB거리 2.py
+++ 백준/Gold/17404. RGB거리 2/RGB거리 2.py
@@ -5,24 +5,15 @@ box = [tuple(map(int,input().split())) for _ in range(n)]
 dp = [[0]*3 for _ in range(n)]
 ans = INF
 
-def DP(num,end):
-    global ans, dp
-    dp[0][num] = box[0][num]
-    dp[0][(num+1)%3] = INF
-    dp[0][(num+2)%3] = INF
-
-    for i in range(1,n-1):
-        # 빨강
-        dp[i][0] = box[i][0] + min(dp[i-1][1], dp[i-1][2])
-        # 초록
-        dp[i][1] = box[i][1] + min(dp[i-1][0], dp[i-1][2])
-        # 파랑
-        dp[i][2] = box[i][2] + min(dp[i-1][0], dp[i-1][1])
-    
-    ans = min(ans, box[-1][end] + dp[n-2][(end+1)%3], box[-1][end] + dp[n-2][(end+2)%3])
-
 for i in range(3):
-    DP(i,(i+1)%3)
-    DP(i,(i+2)%3)
+    dp = [[INF] * 3 for _ in range(n)]
+    dp[0][i] = box[0][i]
+    for j in range(1,n):
+        dp[j][0] = box[j][0] + min(dp[j-1][1], dp[j-1][2])
+        dp[j][1] = box[j][1] + min(dp[j-1][0], dp[j-1][2])
+        dp[j][2] = box[j][2] + min(dp[j-1][0], dp[j-1][1])
     
+    dp[-1][i] = INF
+    ans = min(ans, min(dp[-1]))
+
 print(ans)
