29d21f0bfd39040b12f5523d701153bc6b22b391
JungWook Mun <68419544+m020202@users.noreply.github.com>
2024-08-02 14:27:10 +0900

    Update ACM Craft.py

--- 백준/Gold/1005. ACM Craft/ACM Craft.py
+++ 백준/Gold/1005. ACM Craft/ACM Craft.py
@@ -1,34 +1,42 @@
-import sys
 from collections import deque
- 
-T=int(sys.stdin.readline())
- 
-for _ in range(T):
-    N,K=map(int,sys.stdin.readline().split())#건물수, 건설순서규칙
-    time=[0]+list(map(int,sys.stdin.readline().split()))#건물들의 건설시간
-    seq=[[] for _ in range(N+1)]#건설순서규칙
-    inDegree=[0 for _ in range(N+1)]#진입차수
-    DP=[0 for _ in range(N+1)]#해당 건물까지 걸리는 시간
- 
-    for _ in range(K):#건설순서규칙 저장
-        a,b=map(int,sys.stdin.readline().split())
-        seq[a].append(b)
-        inDegree[b]+=1
- 
-    q = deque()
-    for i in range(1,N+1):#진입차수 0인거 찾아서 큐에 넣기
-        if inDegree[i]==0:
-            q.append(i)
-            DP[i]=time[i]
- 
-    while q:
-        a=q.popleft()
-        for i in seq[a]:
-            inDegree[i]-=1#진입차수 줄이고
-            DP[i]=max(DP[a]+time[i],DP[i])#DP를 이용해 건설비용 갱신
-            if inDegree[i]==0:
-                q.append(i)
- 
- 
-    ans=int(sys.stdin.readline())
-    print(DP[ans])
+import sys
+input = sys.stdin.readline
+t = int(input())
+for _ in range(t):
+    n,k = map(int,input().split())
+    time = list(map(int,input().split()))
+    time.insert(0,0)
+    # 먼저 필요한 건물 리스트
+    dic = {}
+    # 먼저 필요한 건물 개수
+    cnt = [0] * (n+1)
+    for i in range(1,n+1):
+        dic[i] = []
+    for _ in range(k):
+        x,y = map(int,input().split())
+        dic[x].append(y)
+        cnt[y] += 1
+
+    w = int(input())
+
+    # 각 건물마다 최종 건설 시간
+    dp = [0] * (n+1)
+    dQ = deque()
+    for i in range(1,n+1):
+        if  cnt[i] == 0:
+            dQ.append(i)
+            dp[i] = time[i]
+
+    while dQ:
+        cur = dQ.popleft()
+
+        for i in dic[cur]:
+            cnt[i] -= 1
+            dp[i] = max(dp[cur] + time[i], dp[i])
+            if cnt[i] == 0:
+                dQ.append(i)
+        
+    print(dp[w])
+
+                
+        
