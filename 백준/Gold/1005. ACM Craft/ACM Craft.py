3856f3e02c25256a209a928814078163d2eef4f6
JungWook Mun <68419544+m020202@users.noreply.github.com>
2024-08-02 14:25:11 +0900

    [Gold III] Title: ACM Craft, Time: 1344 ms, Memory: 34472 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.py
+++ 백준/Gold/1005. ACM Craft/ACM Craft.py
@@ -1,48 +1,34 @@
+import sys
 from collections import deque
-t = int(input())
-for _ in range(t):
-    n,k = map(int,input().split())
-    time = list(map(int,input().split()))
-    time.insert(0,0)
-    # 먼저 필요한 건물 리스트
-    dic = {}
-    # 먼저 필요한 건물 개수
-    cnt = [0] * (n+1)
-    # 후에 지을 수 있는 건물 리스트
-    outDic = {}
-    for i in range(1,n+1):
-        dic[i] = []
-        outDic[i] = []
-    for _ in range(k):
-        x,y = map(int,input().split())
-        dic[y].append(x)
-        outDic[x].append(y)
-    for i in range(1,n+1):
-        cnt[i] = len(dic[i])
-    w = int(input())
-
-    # 각 건물마다 최종 건설 시간
-    dp = [0] * (n+1)
-    dQ = deque()
-    for i in range(1,n+1):
-        if len(dic[i]) == 0:
-            dQ.append(i)
-
-    while dQ:
-        cur = dQ.popleft()
-        dp[cur] = time[cur]
-        if len(dic[cur]) > 0:
-            mx = 0
-            for i in dic[cur]:
-                mx = max(mx, dp[i])
-            dp[cur] += mx
-
-        for i in outDic[cur]:
-            cnt[i] -= 1
-            if cnt[i] == 0:
-                dQ.append(i)
-        
-    print(dp[w])
-
-                
-        
+ 
+T=int(sys.stdin.readline())
+ 
+for _ in range(T):
+    N,K=map(int,sys.stdin.readline().split())#건물수, 건설순서규칙
+    time=[0]+list(map(int,sys.stdin.readline().split()))#건물들의 건설시간
+    seq=[[] for _ in range(N+1)]#건설순서규칙
+    inDegree=[0 for _ in range(N+1)]#진입차수
+    DP=[0 for _ in range(N+1)]#해당 건물까지 걸리는 시간
+ 
+    for _ in range(K):#건설순서규칙 저장
+        a,b=map(int,sys.stdin.readline().split())
+        seq[a].append(b)
+        inDegree[b]+=1
+ 
+    q = deque()
+    for i in range(1,N+1):#진입차수 0인거 찾아서 큐에 넣기
+        if inDegree[i]==0:
+            q.append(i)
+            DP[i]=time[i]
+ 
+    while q:
+        a=q.popleft()
+        for i in seq[a]:
+            inDegree[i]-=1#진입차수 줄이고
+            DP[i]=max(DP[a]+time[i],DP[i])#DP를 이용해 건설비용 갱신
+            if inDegree[i]==0:
+                q.append(i)
+ 
+ 
+    ans=int(sys.stdin.readline())
+    print(DP[ans])
