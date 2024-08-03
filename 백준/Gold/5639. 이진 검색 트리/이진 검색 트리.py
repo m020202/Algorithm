7d895b4cbbabf1caaef1947c4a8daf85a4fc661d
JungWook Mun <68419544+m020202@users.noreply.github.com>
2024-08-03 17:00:24 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 2544 ms, Memory: 424768 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
@@ -1,41 +1,29 @@
 import sys
-input = sys.stdin.readline
 sys.setrecursionlimit(65536)
-tree = []
-left = [-1] * (10001)
-right = [-1] * (10001)
-tree.append(int(input()))
-idx = 1
+input = sys.stdin.readline
 
-def constructing(idx,cur):
-    if tree[idx] > cur:
-        if left[idx] == -1:
-            tree.append(cur)
-            left[idx] = len(tree)-1
-            return len(tree)-1
-        else:
-            return constructing(left[idx], cur)
-    else:
-        if right[idx] == -1:
-            tree.append(cur)
-            right[idx] = len(tree) -1
-            return len(tree)-1
-        else:
-            return constructing(right[idx],cur)
-
+box = []
 while True:
     try:
-        cur = int(input())
-        constructing(0,cur)               
+        box.append(int(input()))
     except:
         break
-            
-# 후위 순회
-def postOrder(idx):
-    if (left[idx] != -1):
-        postOrder(left[idx])
-    if (right[idx] != -1):
-        postOrder(right[idx])
-    print(tree[idx])
 
-postOrder(0)
+def postOrder(arr):
+    if len(arr) == 0:
+        return
+    mid = arr[0]
+    left = []
+    right = []
+    for i in range(1,len(arr)):
+        if arr[i] > mid:
+            left = arr[1:i]
+            right = arr[i:]
+            break
+    else:
+        left = arr[1:]
+    postOrder(left)
+    postOrder(right)
+    print(mid)
+    
+postOrder(box)
