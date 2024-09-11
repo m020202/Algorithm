e83f2fc3884c37eb5a62e865b927f46d537d618a
JungWook Mun <68419544+m020202@users.noreply.github.com>
2024-09-11 22:19:58 +0900

    [Gold IV] Title: 소트, Time: 52 ms, Memory: 31696 KB -BaekjoonHub

--- 백준/Gold/1083. 소트/소트.py
+++ 백준/Gold/1083. 소트/소트.py
@@ -3,46 +3,19 @@ input = sys.stdin.readline
 
 n = int(input())
 box = list(map(int,input().split()))
-srt = copy.deepcopy(box)
-ch = [0] * n
-
-def sorting():
-    global box,srt,ch
-    srt = copy.deepcopy(box)
-    srt.sort(reverse=True)
-    ch = [0] * n
-    for i in range(n):
-        cur = srt[i]
-        ch[i] = box.index(cur)
-
 s = int(input())
 
-sorting()
-cnt = 0
-while s > 0 and cnt < n:
-    # 숫자의 인덱스
-    idx = 0
-    # 바꿀 대상
-    num = box[cnt]
-    c = srt.index(num)
-    # 실제 box에서의 위치
-    tf = False
-    for i in range(c):
-        if 0<= ch[i] - cnt <= s:
-            idx = ch[i]
-            tf = True
-            break
-    if not tf:
-        cnt += 1
-        continue
-    for i in range(idx,cnt,-1):
-        tmp = box[i]
-        box[i] = box[i-1]
-        box[i-1] = tmp
-    
-    sorting()
-    s -= (idx-cnt)
-    cnt += 1
+for i in range(n):
+    max_num = max(box[i:min(n, i + s + 1)])
+    idx = box.index(max_num)
 
-for i in box:
-    print(i,end=' ')
+    for j in range(idx,i,-1):
+        box[j],box[j-1] = box[j-1],box[j]
+    
+    s -= (idx-i)
+    if s <= 0:
+        break
+
+print(*box)
+
+
