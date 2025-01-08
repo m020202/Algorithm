228b7255d1055fba82dc747e42577192e2e8f031
JungWook Mun <68419544+m020202@users.noreply.github.com>
2025-01-08 19:23:40 +0900

    [Gold IV] Title: 좋다, Time: 148 ms, Memory: 110896 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.py
+++ 백준/Gold/1253. 좋다/좋다.py
@@ -1,39 +1,35 @@
+import sys
+input = sys.stdin.readline
 n = int(input())
 box = list(map(int,input().split()))
 box.sort()
 
+dic = {}
 ch = {}
-cnt = 0
-for i in range(len(box)):
-    if box[i] in ch:
-        cnt += 1
-        continue
-    lt = 0
-    rt = 1
-    while lt < n-1:
-        if lt == i:
-            lt += 1
-            rt = lt + 1
-            continue
-        if rt == i:
-            rt += 1
-            if rt == n:
-                lt += 1
-                rt = lt + 1
-                continue
-        num = box[lt] + box[rt]
-        if num == box[i]:
-            cnt += 1
-            ch[box[i]] = 1
+for i in box:
+    if i in dic.keys():
+        dic[i] += 1
+    else:
+        dic[i] = 1
+        ch[i] = 0
 
-            break
-        elif num < box[i]:
-            rt += 1
-            if rt == n:
-                lt += 1
-                rt = lt + 1
-        else:
-            lt += 1
-            rt = lt + 1
-print(cnt)
+s = set(box)
+ans = 0
+for i in range(n):
+    for j in range(i+1, n):
+        if box[i] == 0 and box[j] == 0:
+            if dic[0] < 3:
+                continue
+        elif box[i] == 0 and box[j] != 0:
+            if dic[box[j]] == 1:
+                continue
+        elif box[i] != 0 and box[j] == 0:
+            if dic[box[i]] == 1:
+                continue
+        cur = box[i] + box[j]
+        if cur in s and ch[cur] == 0:
+            ch[cur] = 1
+            ans += dic[cur]
+
+print(ans)
 
