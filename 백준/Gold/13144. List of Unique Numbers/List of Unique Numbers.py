fb8b55ae00723398fe74ba37afa9a1e2d735a971
JungWook Mun <68419544+m020202@users.noreply.github.com>
2025-01-11 20:52:13 +0900

    [Gold IV] Title: List of Unique Numbers, Time: 108 ms, Memory: 122160 KB -BaekjoonHub

---
+++ 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.py
@@ -0,0 +1,17 @@
+n = int(input())
+box = list(map(int,input().split()))
+st = 0
+en = 0
+ch = [0] * 100001
+ans = 0
+while st <= en and en < n:
+    if ch[box[en]] == 0:
+        ch[box[en]] = 1
+        en += 1
+        ans += en - st
+    else:
+        while ch[box[en]]:
+            ch[box[st]] = 0
+            st += 1
+
+print(ans)
