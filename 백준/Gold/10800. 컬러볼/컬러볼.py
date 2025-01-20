d563bece3dc8f5a64acc35c29f5254d2ad4a9cc6
JungWook Mun <68419544+m020202@users.noreply.github.com>
2025-01-20 10:33:12 +0900

    [Gold II] Title: 컬러볼, Time: 448 ms, Memory: 151684 KB -BaekjoonHub

--- 백준/Gold/10800. 컬러볼/컬러볼.py
+++ 백준/Gold/10800. 컬러볼/컬러볼.py
@@ -1,49 +1,24 @@
-import sys, copy
+import sys
 input = sys.stdin.readline
 n = int(input())
-box = [tuple(map(int,input().split())) for _ in range(n)]
 
-# 정렬용
-tmp = copy.deepcopy(box)
-tmp.sort(key = lambda x : x[1])
-
-# 각 튜플의 실제 위치
-idx = {}
-for i in range(n-1,-1,-1):
-    idx[(tmp[i][0],tmp[i][1])] = i
-
-k = set([tmp[i][0] for i in range(n)])
-
-# 공 색깔에 속하는 크기들
-dic = {}
-for i in k:
-    dic[i] = []
-
-# 같은 색깔에 대한 본인 전까지 누적합
-same = {}
+# 값들 입력 받고, 인덱스까지 포함해서 튜플로 넣기
+box = []
 for i in range(n):
-    left = tmp[i][0]
-    right = tmp[i][1]
-    if len(dic[left]) == 0:
-        same[(left,right)] = 0
-    elif dic[left][-1] < right:
-        same[(left, right)] = sum(dic[left])
-    else:
-        same[(left,right)] = same[(left,dic[left][-1])]
-    dic[left].append(right)
+    c,s = map(int,input().split())
+    box.append((c,s,i))
+box.sort(key = lambda x : x[1])
 
-# 총 누적합
-ch = [0] * n
-temp = [tmp[i][1] for i in range(n)]
-for i in range(1,n):
-    if tmp[i][1] == tmp[i-1][1]:
-        ch[i] = ch[i-1]
-    else:
-        ch[i] = sum(temp[:i])
-
+INF = 200001
+cnt = [0] * INF
+tot = 0
+j = 0
+ans = [0] * n
 for i in range(n):
-    c,s = box[i][0], box[i][1]
-    cur = idx[(c,s)]
-    print(ch[cur] - same[(c,s)])
+    while box[j][1] < box[i][1]:
+        cnt[box[j][0]] += box[j][1]
+        tot += box[j][1]
+        j += 1
+    ans[box[i][2]] = tot - cnt[box[i][0]]
 
-
+[print(i) for i in ans]
