c2c4bb37f574b30a54ca548cf0bc24c52d96dc24
JungWook Mun <68419544+m020202@users.noreply.github.com>
2025-06-23 10:16:43 +0900

    [Gold IV] Title: 최단경로, Time: 480 ms, Memory: 136104 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,61 +1,36 @@
-import sys
+import sys, heapq
 input = sys.stdin.readline
 INF = sys.maxsize
+n,e = map(int,input().split())
+k = int(input())
 
-n,m = map(int,input().split())
-src = int(input())
-src -= 1
-
-box = {}
-for i in range(n):
-    box[i] = {}
-
-# 0: no, 1: fringe, 2: tree
-status = [0] * n
-
-value = [-1] * n
-
-for _ in range(m):
+# 각 정점별 갈 수 있는 경로 정의
+dic = {}
+for i in range(1,n+1):
+    dic[i] = []
+for _ in range(e):
     u,v,w = map(int,input().split())
-    u -= 1
-    v -= 1
-    if v in box[u]:
-        box[u][v] = min(box[u][v],  w)
-    else:
-        box[u][v] = w
+    dic[u].append((v,w))
 
-status[src] = 2
-value[src] = 0
+ans = [INF] * (n+1)
+ans[k] = 0
 
-# src와 인접한 V들 fringe로 바꾸기
-for i in box[src]:
-    status[i] = 1
-    value[i] = box[src][i]
+heap = []
+heapq.heappush(heap,(0,k))
+while heap:
+    curW, curV = heapq.heappop(heap)
+    if ans[curV] < curW:
+        continue
+    for v,w in dic[curV]:
+        if ans[v] > w + ans[curV]:
+            ans[v] = w + ans[curV]
+            heapq.heappush(heap,(ans[v],v))
 
-while True:
-    tmp = False
-    mn = INF
-    idx = -1
-    for i in range(n):
-        if status[i] == 1:
-            if value[i] < mn:
-                mn = value[i]
-                idx = i
-                tmp = True
-    if tmp == False:
-        break
-
-    cur = mn
-    status[idx] = 2
-    for i in box[idx]:
-        if status[i] == 1:
-            value[i] = min(value[i], cur + box[idx][i])
-        elif status[i] == 0:
-            status[i] = 1
-            value[i] = cur + box[idx][i]
-
-for i in value:
-    if i == -1:
+for i in range(1,n+1):
+    if ans[i] == INF:
         print("INF")
     else:
-        print(i)
+        print(ans[i])
+
+
+
