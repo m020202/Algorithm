ece0425b8d54ad3105e3f3d2e50bee4330b42be5
JungWook Mun <68419544+m020202@users.noreply.github.com>
2025-07-04 12:28:45 +0900

    [Gold V] Title: 치킨 배달, Time: 188 ms, Memory: 115632 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -1,59 +1,46 @@
-# 0: 빈칸, 1: 집, 2: 치킨집
 import sys
+from collections import deque
+input = sys.stdin.readline
 INF = sys.maxsize
 n,m = map(int,input().split())
-box = []
-hX = []
-hY = []
-cX = []
-cY = []
-for _ in range(n):
-    box.append(list(map(int,input().split())))
+box = [list(map(int,input().split())) for _ in range(n)]
 
+home = []
+chicken = []
 for i in range(n):
     for j in range(n):
         if box[i][j] == 1:
-            hX.append(i)
-            hY.append(j)
-        
+            home.append((i,j))
         if box[i][j] == 2:
-            cX.append(i)
-            cY.append(j)
-    
-ch = [[0] * len(cX) for _ in range(len(hX))]
+            chicken.append((i,j))
 
-for i in range(len(hX)):
-    for j in range(len(cX)):
-        num = abs(hX[i]-cX[j]) + abs(hY[i] - cY[j])
-        ch[i][j] = num
-
-array = list(range(len(cX)))
-used = [False for _ in range(len(array))]
-
-def checking(ch, arr):
-    array = [[INF] * len(cX) for _ in range(len(hX))]
-    for i in range(len(hX)):
-        for j in arr:
-            array[i][j] = ch[i][j]
-        
-    ans = 0
-    for i in array:
-        ans += min(i)
-    return ans
-
+dx = [1,0,-1,0]
+dy = [0,1,0,-1]
 ans = INF
-def back_comb(idx,arr):
+
+# 거리 계산 함수
+def calc(chosen):
     global ans
-    if len(arr) == m:
-        cur = checking(ch, arr)
-        ans = min(ans,cur)
+    tot = 0
+    for i,j in home:
+        cur = INF
+        for x,y in chosen:
+            cur = min(cur, abs(x-i) + abs(y-j))
+        tot += cur
+    ans = min(ans,tot)
+
+# 치킨집 선택 함수
+def choose(chosen, num, idx):
+    if num == m:
+        calc(chosen)
         return
 
-    for i in range(idx+1,len(array)):
-        if used[i] == False:
-            used[i] = True
-            back_comb(i, arr + [array[i]])
-            used[i] = False
+    for i in range(idx+1, len(chicken)):
+        chosen.add(chicken[i])
+        choose(chosen, num+1, i)
+        chosen.remove(chicken[i])
 
-back_comb(-1,[])
+choose(set(),0,-1)
 print(ans)
+
+
