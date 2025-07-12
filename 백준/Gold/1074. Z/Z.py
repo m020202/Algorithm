0db8c1811b961ae9f47efc5952954e85bf42abe2
JungWook Mun <68419544+m020202@users.noreply.github.com>
2025-07-12 17:19:31 +0900

    [Gold V] Title: Z, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Gold/1074. Z/Z.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+n,r,c = map(int,input().split())
+num = 2**n
+k = 2**(n-1)
+size = k*k # 면적 크기
+# 2단계 - k*k 내부 실제 위치 찾기
+def func(dep,x,y,cnt):
+    if dep==1:
+        return x,y,cnt
+    else:
+        off_x = (r-x)//dep
+        new_x = (off_x*dep) + x
+        off_y = (c-y)//dep
+        new_y = (off_y*dep) + y
+        new_cnt = (off_x*2 + off_y) * (dep**2) + cnt
+        return func(dep//2,new_x,new_y,new_cnt)
+
+def calc(x,y,cnt):
+    dx = [0, 0, 1, 1]
+    dy = [0, 1, 0, 1]
+    for i in range(4):
+        xx = x + dx[i]
+        yy = y + dy[i]
+        if xx==r and yy==c:
+            print(cnt+i)
+
+x, y, cnt = func(k, 0, 0, 0)
+calc(x, y, cnt)
