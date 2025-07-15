cf8cdb6c61606e7687ac15e6768bce71881a5069
JungWook Mun <68419544+m020202@users.noreply.github.com>
2025-07-15 13:07:20 +0900

    [Gold IV] Title: 빙산, Time: 556 ms, Memory: 174056 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -1,70 +1,69 @@
+import sys
 from collections import deque
+input = sys.stdin.readline
 n,m = map(int,input().split())
+box = [list(map(int,input().split())) for _ in range(n)]
+dx = [1,0,-1,0]
+dy = [0,1,0,-1]
 
-box = []
-
-for _ in range(n):
-    spot = list(map(int,input().split()))
-    box.append(spot)
-
-def checking():
-    cnt = 0
-    ch = [[0]*m for _ in range(n)]
-    dx = [1,0,-1,0]
-    dy = [0,1,0,-1]
-
+def checking(ch): # 두 덩어리 이상 체크
+    tmp = [[0]*m for _ in range(n)]
+    cnt = 1
     for i in range(n):
         for j in range(m):
-            if box[i][j] != 0 and ch[i][j] == 0:
-                dQ = []
-                dQ = deque(dQ)
-                dQ.append((i,j))
-                ch[i][j] = 1
-                cnt+= 1
-
-                while(dQ):
-                    x,y = dQ.popleft()
-                    
+            if ch[i][j] > 0 and tmp[i][j] == 0:
+                if cnt > 1:
+                    return True
+                tmp[i][j] = cnt
+                dq = deque([(i,j)])
+                while dq:
+                    x,y = dq.popleft()
                     for k in range(4):
                         xx = x + dx[k]
                         yy = y + dy[k]
                         if 0<=xx<n and 0<=yy<m:
-                            if box[xx][yy] != 0 and ch[xx][yy] == 0:
-                                ch[xx][yy] = 1
-                                dQ.append((xx,yy))
-    return cnt
+                            if ch[xx][yy] > 0 and tmp[xx][yy] == 0:
+                                tmp[xx][yy] = cnt
+                                dq.append((xx,yy))
+                cnt += 1
+    return False
 
-dx = [1,0,-1,0]
-dy = [0,1,0,-1]
+island = {}
+for i in range(n):
+    for j in range(m):
+        if box[i][j] > 0:
+            island[(i,j)] = True
 
-
-tot = 0
+year = 0
+res = True
 while True:
-    tmp = True
-
-    bh = [[0] * m for _ in range(n)]
-    for i in range(n):
-        for j in range(m):
-            if box[i][j] > 0:
-                tmp = False
-                cnt = 0
-                for k in range(4):
-                    ii = i + dx[k]
-                    jj = j + dy[k]
-                    if 0<=ii<n and 0<=jj<m and box[ii][jj] == 0 and bh[ii][jj] == 0:
-                        cnt += 1
-                if box[i][j]-cnt <= 0:
-                    box[i][j] = 0
-                    bh[i][j] = 1
-                else:
-                    box[i][j] -= cnt
-    
-    tot += 1
-
-    if tmp == True:
+    if res and checking(box):
+        print(year)
+        sys.exit()
+    if not island:
         print(0)
-        break
-    
-    if checking() >= 2:
-        print(tot)
-        break
+        sys.exit()
+
+    tmp = []
+    for i,j in island:
+        cnt = 0
+        for k in range(4):
+            ii = i + dx[k]
+            jj = j + dy[k]
+            if n > ii >= 0 == box[ii][jj] and 0<=jj<m:
+                cnt += 1
+
+        new = box[i][j] - cnt if box[i][j] > cnt else 0
+        tmp.append((i,j,new))
+
+    res = False
+    for i,j,new in tmp:
+        box[i][j] = new
+        if new == 0:
+            res = True
+            island.pop((i,j))
+
+    year += 1
+
+
+
