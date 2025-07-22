39f4d1b137424da90167c1d19a9e1133d1dce139
JungWook Mun <68419544+m020202@users.noreply.github.com>
2025-07-22 20:57:37 +0900

    [Gold III] Title: 사다리 조작, Time: 6012 ms, Memory: 112176 KB -BaekjoonHub

--- 백준/Gold/15684. 사다리 조작/사다리 조작.py
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.py
@@ -1,55 +1,63 @@
 import sys
 input = sys.stdin.readline
 n,m,h = map(int,input().split())
-box = [[0] * n for _ in range(h)]
+ch = [[0]* n for _ in range(h+1)]
+dic = {i : {} for i in range(1,h+1)}
 for _ in range(m):
-    x,y = map(int,input().split())
-    x -= 1
-    y -= 1
-    box[x][y] = 1
+    a,b = map(int,input().split())
+    dic[a][b] = True
+    ch[a][b] = 1
 
-# 탐색하기
-def searching():
-    for i in range(n):
-        x,y = 0,i
-        while x < h:
-            if box[x][y] == 1:
-                y += 1
-            elif box[x][y-1] == 1:
-                y -= 1
-            x += 1
-        if y != i:
+def checking():
+    for i in range(1,n+1):
+        cur = i
+        for j in range(1,h+1):
+            if cur in dic[j]:
+                cur += 1
+            elif cur-1 in dic[j]:
+                cur -= 1
+        if cur != i:
             return False
     return True
 
-# 0일 때
-if searching():
+# 0인 경우
+if checking():
     print(0)
     sys.exit()
 
-ans = -1
-def checking(cnt):
-    global ans
-    if cnt == 3:
-        return
+# 1, 2, 3인 경우
+is3 = False
+def dfs(cur,l,k):
+    global is3
+    if cur == 3:
+        if checking():
+            is3 = True
     else:
-        for i in range(h):
-            for j in range(n-1):
-                if j >= 1 == box[i][j - 1]:
-                    continue
-                if box[i][j] == 0:
-                    box[i][j] = 1
-                    if searching():
-                        if cnt == 0:
-                            print(1)
-                            sys.exit()
-                        if ans == -1:
-                            ans = cnt + 1
-                        else:
-                            ans = min(ans, cnt + 1)
-                    checking(cnt + 1)
-                    box[i][j] = 0
-checking(0)
+        if cur == 1:
+            if checking():
+                print(1)
+                sys.exit()
+        if cur == 2:
+            if checking():
+                print(2)
+                sys.exit()
 
-print(ans)
+        for i in range(k+1,n):
+            if ch[l][i] == 0:
+                ch[l][i] = 1
+                dic[l][i] = True
+                dfs(cur+1,l,i)
+                ch[l][i] = 0
+                dic[l].pop(i)
 
+        for i in range(l+1,h+1):
+            for j in range(1,n):
+                if ch[i][j] == 0:
+                    ch[i][j] = 1
+                    dic[i][j] = True
+                    dfs(cur+1,i,j)
+                    ch[i][j] = 0
+                    dic[i].pop(j)
+
+dfs(0,1,0)
+print(3 if is3 else -1)
