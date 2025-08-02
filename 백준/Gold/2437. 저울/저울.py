3a4c8c13247e67b943f425cc43027b7465a00236
JungWook Mun <68419544+m020202@users.noreply.github.com>
2025-08-02 14:30:44 +0900

    [Gold II] Title: 저울, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2437. 저울/저울.py
+++ 백준/Gold/2437. 저울/저울.py
@@ -1,13 +1,12 @@
 import sys
 input = sys.stdin.readline
 n = int(input())
-arr = list(map(int, input().split()))
-arr.sort()
+box = list(map(int, input().split()))
+box.sort()
 
-target = 1
-for num in arr:
-    if target < num:
+cur = 1
+for i in box:
+    if cur < i:
         break
-    target += num
-
-print(target)
+    cur += i
+print(cur)
