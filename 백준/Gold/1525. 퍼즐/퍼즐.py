bfa24ebd3ac4a7081c68721cbf23d4af7c659d16
JungWook Mun <68419544+m020202@users.noreply.github.com>
2025-08-14 12:28:09 +0900

    [Gold II] Title: 퍼즐, Time: 604 ms, Memory: 192468 KB -BaekjoonHub

--- 백준/Gold/1525. 퍼즐/퍼즐.py
+++ 백준/Gold/1525. 퍼즐/퍼즐.py
@@ -1,22 +1,22 @@
-def to_tuple(matrix):
-    new_tuple = ()
+def to_string(matrix):
+    new_string = ""
     for i in range(3):
         for j in range(3):
-            new_tuple += (matrix[i][j],)
-    return new_tuple
+            new_string += str(matrix[i][j])
+    return new_string
 
 
 import sys
 from collections import deque
 input = sys.stdin.readline
 arr = [list(map(int,input().split())) for _ in range(3)]
-ans = (1,2,3,4,5,6,7,8,0)
+ans = "123456780"
 dx = [1,0,-1,0]
 dy = [0,1,0,-1]
 tot = 100000
 ch = {}
 
-tmp = to_tuple(arr)
+tmp = to_string(arr)
 ch[tmp] = 0
 x = y = 0
 for i in range(3):
@@ -27,13 +27,13 @@ for i in range(3):
 dq = deque([(x,y,0,tmp)])
 while dq:
     x,y,cnt,t = dq.popleft()
-    box = [list(t[i:i+3]) for i in range(0,9,3)]
+    box = [list(map(int,t[i:i+3])) for i in range(0,9,3)]
     for i in range(4):
         xx = x + dx[i]
         yy = y + dy[i]
         if 0<=xx<3 and 0<=yy<3:
             box[x][y], box[xx][yy] = box[xx][yy], box[x][y]
-            tmp = to_tuple(box)
+            tmp = to_string(box)
             if (tmp in ch and ch[tmp]>cnt+1) or tmp not in ch:
                 ch[tmp] = cnt+1
                 dq.append((xx,yy,cnt+1,tmp))
@@ -43,5 +43,6 @@ if ans in ch:
     print(ch[ans])
 else:
     print(-1)
+
 
 
