d8eb48f3119382b0f2db61873fdfd066011b7cd5
JungWook Mun <68419544+m020202@users.noreply.github.com>
2025-10-17 19:46:35 +0900

    [Gold II] Title: 합이 0인 네 정수, Time: 6820 ms, Memory: 618244 KB -BaekjoonHub

--- 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.py
+++ 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.py
@@ -9,20 +9,39 @@ for _ in range(n):
     C.append(c)
     D.append(d)
 
-a_b = {}
+A_B = []
 for i in range(n):
     for j in range(n):
-        num = A[i] + B[j]
-        if num in a_b:
-            a_b[num] += 1
-        else:
-            a_b[num] = 1
+        A_B.append(A[i]+B[j])
+A_B.sort()
+
+C_D = []
+for i in range(n):
+    for j in range(n):
+        C_D.append(C[i]+D[j])
+C_D.sort()
 
 ans = 0
-for i in range(n):
-    for j in range(n):
-        cur = C[i] + D[j]
-        if -cur in a_b:
-            ans += a_b[-cur]
+lt,rt = 0,len(C_D)-1
+while lt < len(A_B) and rt >= 0:
+    mid = A_B[lt] + C_D[rt]
+    if mid > 0:
+        rt -= 1
+    elif mid < 0:
+        lt += 1
+    else:
+        cur = A_B[lt]
+        l_cnt = 0
+        while lt < len(A_B) and A_B[lt] == cur:
+            l_cnt += 1
+            lt += 1
+
+        cur = C_D[rt]
+        r_cnt = 0
+        while rt >= 0 and C_D[rt] == cur:
+            r_cnt += 1
+            rt -= 1
+
+        ans += (l_cnt*r_cnt)
 
 print(ans)
